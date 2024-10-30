af563693bd7296e9a5abed7f1f847783daf784bc
kouta-takagi <k-takagi@ruby-dev.jp>
2024-10-30 11:25:24 +0900

    640pxから動かない理由がわからない

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
     <>
       <header>
-        <Link href="/" className="font-bold text-xl no-underline">
-          通販アプリ
-        </Link>
-        <Link href=""></Link>
-        <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+        <div className="flex justify-between w-full">
+          <Link href="/" className="font-bold text-xl no-underline">
+            通販アプリ
+          </Link>
+          <Link href="/cart_items">
+            <FontAwesomeIcon icon={faCartShopping} className="size-6" />
+          </Link>
+        </div>
       </header>
-      <div></div>
+      <div className="flex justify-between">
+        <p>ka</p>
+        <p>sa</p>
+      </div>
       <footer></footer>
     </>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,10 +14,14 @@ const config: Config = {
       },
     },
   },
+  corePlugins: {
+    aspectRatio: false,
+  },
   plugins: [
     require("daisyui"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
   ],
 };
 export default config;
