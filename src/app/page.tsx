7d5ffa5704d39e5dba5a88b61649d0fe5094b6f2
kouta-takagi <k-takagi@ruby-dev.jp>
2024-10-30 11:05:21 +0900

    reactがリリース候補版のせいでコンフリクト発生

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <header></header>
-      <div>
-        <button className=""></button>
-        <button className="btn btn-neutral">Neutral</button>
-        <button className="btn btn-neutral">Neutral</button>
-        <article>
-          <h1>Garlic bread with cheese: What the science tells us</h1>
-          <p>
-            For years parents have espoused the health benefits of eating garlic
-            bread with cheese to their children, with the food earning such an
-            iconic status in our culture that kids will often dress up as warm,
-            cheesy loaf for Halloween.
-          </p>
-          <p>
-            But a recent study shows that the celebrated appetizer may be linked
-            to a series of rabies cases springing up around the country.
-          </p>
-          <h1>滉高木</h1>
-          <input type="checkbox" className="rounded text-pink-500" />
-          <input type="email" class="form-input rounded-full px-4 py-3" />
-        </article>
-      </div>
+      <header>
+        <Link href="/" className="font-bold text-xl no-underline">
+          通販アプリ
+        </Link>
+        <Link href=""></Link>
+        <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+      </header>
+      <div></div>
       <footer></footer>
     </>
   );
